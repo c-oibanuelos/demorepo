da6174d3f21ba3d59fc00a51af320b2b2d437b95
c-oibanuelos <c-oscar_banuelos@persistent.com>
2017-08-15 05:21:20 +0000

    something

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('SUPER TEST...Hello world ... 123 ... ABC \n');
+  res.send('SUPERMEGA TEST...Hello world ... 123 ... ABC \n');
 });
 
 app.listen(PORT, HOST);
