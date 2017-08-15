39909c5b55a43af7a764e379f59001ce3a852c99
Unknown <c-oscar_banuelos@persistent.com>
2017-08-15 12:14:05 -0400

    change again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'This is Oscar Demo. Angular, Node and Twitter API' });
+  res.render('index', { title: 'Another change. Angular, Node and Twitter API' });
 });
 
 module.exports = router;
