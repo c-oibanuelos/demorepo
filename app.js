1877210ad600f64a5c1a16983e673b43afceb870
Unknown <c-oscar_banuelos@persistent.com>
2017-08-15 09:26:53 -0400

    some big changes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var path = require('path');
+var index = require('./routes/index');
+var tweets = require('./routes/tweets');
 var app = express();
-var router = express.Router();
 
-router.get('/',function(req,res) {
-    res.send("UTLTIMATE MEGA SUPER Hello World");
+// serve static assets from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// look for view html in the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// use ejs to render 
+app.set('view engine', 'ejs');
+
+// setup routes
+app.use('/', index);
+app.use('/tweets', tweets);
+
+
+module.exports = app;
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log('Listening on ' + port);
 });
-
-app.use('/',router);
-
-app.listen(9090);
