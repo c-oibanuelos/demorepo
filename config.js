f7806fca97213b9e9de640cb32a6443bd45409ab
Oscar Banuelos <c-oscar_banuelos@persistent.com>
2017-08-15 15:50:20 +0000

    test2

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+        twitter: {
+        consumer_key: 'czTvQSSwtulZgFx0lIkg4STl3',
+                consumer_secret: 'gTF79V3APzTHUDxwMJ2a4XXdY2lyQur1RoAezx4816UfCIHMp1',
+                access_token: '897459281443815424-mY2IGkrRGGUXVzNtZVaRibO1884sZdH',
+                access_token_secret: 'aGXxiPyTyZPmUTDOOBLU4HsF9XBTBwsKZpenemM5ligLY'
+        }
+
+};
