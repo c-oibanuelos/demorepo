2d16c3e45f5e371373a330c3e095d03537a9bbdd
Unknown <c-oscar_banuelos@persistent.com>
2017-08-21 13:00:27 -0400

    This is a test push to GitHub

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Another change. Angular, Node and Twitter API' });
+  res.render('index', { title: 'This is a change to my Angular application. Angular, Node and Twitter API' });
 });
 
 module.exports = router;
